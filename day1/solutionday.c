9944f60a6cf4b31de6af6bd024076f10eb70c800
proneprune <jcullgert@hotmail.se>
2024-12-04 00:10:47 +0100

    done with part 1

--- day1/solutionday.c
+++ day1/solutionday.c
@@ -10,16 +10,12 @@ int comp(const void* a, const void* b) {
 int *getArrayFromInput()
 {
     
-
     FILE *file_ptr;
     char str[50];
     int *arr1 = (int *)malloc(1000 * sizeof(int));
     int *arr2 = (int *)malloc(1000 * sizeof(int));
     int idx = 0;  // Index to keep track of where to store numbers in arr1 and arr2
     int *arr3 = (int *)malloc(1000 * sizeof(int));
-
-
-
 
     file_ptr = fopen("input.txt", "r");
 
@@ -29,11 +25,6 @@ int *getArrayFromInput()
 
     // Reading stinrg using fgets
     printf("Content of this file are:: \n");
-/*     while (fgets(str, 50, file_ptr) != NULL) {
-
-        
-        //printf("%s", str);
-    } */
 
        while (fgets(str, sizeof(str), file_ptr) != NULL) {
         int num1, num2;
@@ -48,27 +39,12 @@ int *getArrayFromInput()
     }
     fclose(file_ptr);
 
-/*         for (int i = 0; i < idx; i++) {
-        printf("arr1[%d] = %d, arr2[%d] = %d\n", i, arr1[i], i, arr2[i]);
-    } */
-
-
     qsort(arr1, idx, sizeof(int), comp);
     qsort(arr2, idx, sizeof(int), comp);
-
-
-/*     for(int i = 0; i < 1000; i++) {
-        printf("%d\t%d\n", arr1[i], arr2[i]);
-    } 
- */
 
     for(int i = 0; i < 1000; i++) {
         arr3[i] = abs(arr1[i]-arr2[i]);
     }
-/*     printf("%d\n", arr3[1] + "meow");
-    for(int i = 0; i < 1000; i++) {
-        printf("%d\n", arr3[i]);
-    }  */
 
     free(arr1);
     free(arr2);
